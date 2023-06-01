f7da2e3c566e41dd3b62a41efe6e254eb949cbec
Gali Andal <gandal@adobe.com>
2023-06-01 10:19:53 -0400

    add fetch error handling

--- blogs/blocks/preflight/preflight-checks.js
+++ blogs/blocks/preflight/preflight-checks.js
@@ -87,19 +87,24 @@ checks.push({
     };
     const canon = doc.querySelector("link[rel='canonical']");
     const { href } = canon;
-    const resp = await fetch(href, { method: 'HEAD' });
-    if (!resp.ok) {
+    try {
+      const resp = await fetch(href, { method: 'HEAD' });
+      if (!resp.ok) {
+        res.status = false;
+        res.msg = 'Error with canonical reference.';
+      }
+      if (resp.ok) {
+        if (resp.status >= 300 && resp.status <= 308) {
+          res.status = false;
+          res.msg = 'Canonical reference redirects.';
+        } else {
+          res.status = true;
+          res.msg = 'Canonical referenced is valid.';
+        }
+      }
+    } catch (e) {
       res.status = false;
       res.msg = 'Error with canonical reference.';
-    }
-    if (resp.ok) {
-      if (resp.status >= 300 && resp.status <= 308) {
-        res.status = false;
-        res.msg = 'Canonical reference redirects.';
-      } else {
-        res.status = true;
-        res.msg = 'Canonical referenced is valid.';
-      }
     }
 
     return res;
@@ -117,7 +122,7 @@ checks.push({
     const bodySize = doc.documentElement.innerText.replace(/\s/g, '').length;
     if (bodySize > 200) {
       res.status = true;
-      res.msg = 'Body content has a good length. 200 characters';
+      res.msg = 'Body content has a good length.';
     } else {
       res.status = false;
       res.msg = 'Body does not have enough content.';
@@ -133,9 +138,9 @@ checks.push({
   exec: async (doc) => {
     const res = {
       status: true,
-      msg: 'Body size is good.',
+      msg: 'Links are valid.',
     };
-    const links = doc.querySelectorAll('a[href^="/"]');
+    const links = doc.querySelectorAll('body > main a[href]');
 
     let badLink;
     // eslint-disable-next-line no-restricted-syntax
